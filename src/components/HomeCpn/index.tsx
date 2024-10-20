1f59b1d4eb8d37c8d7050fb8d01adb68ed2bd2ce
joey <969295728@qq.com>
2024-10-20 21:31:59 +0800

    feat: add avatar

--- src/components/HomeCpn/index.tsx
+++ src/components/HomeCpn/index.tsx
@@ -1,9 +1,12 @@
+import { TypedCpn } from "@/components/SelfIntroduction";
+
 export default function HomeCpn() {
   return (
     <>
+      <TypedCpn />
       {(() => {
         const 测试字符串 = [];
-        for (let i = 1; i <= 100; i++) {
+        for (let i = 1; i <= 50; i++) {
           测试字符串.push(`这是第${i}行测试字符串`);
         }
         return 测试字符串.map((字符串, 索引) => <div key={索引}>{字符串}</div>);
--- src/components/Nav/NavCpn.tsx
+++ src/components/Nav/NavCpn.tsx
@@ -3,22 +3,32 @@ import { ModeToggle } from "@/components/ModelToggle";
 import { navLinks } from "@/route/navlinks";
 // import clsx from "clsx";
 import Link from "next/link";
+import { Button } from "../ui/button";
 // import { useRouter } from "next/router";
+import Image from "next/image";
+import lava from "@/public/lava.jpg";
 export default function NavCpn() {
   return (
     <header className="sticky top-0 py-3 bg-opacity-80 backdrop-blur-md w-full">
       <div className="flex justify-between items-center max-w-3xl px-3 mx-auto xl:max-w-5xl xl:px-0">
-        <div>
+        <div className="flex items-center gap-2">
+          <Image
+            src={lava}
+            width={50}
+            height={50}
+            alt="Joe's Avatar"
+            className="rounded-full"
+          />
           <Link href="/home" aria-label="Joe's Blog" className="text-xl">
             Joe&apos;s Blog
           </Link>
         </div>
-        <div className="flex items-center text-base leading-5">
+        <div className="flex items-center text-base leading-5 gap-2">
           <div className="hidden space-x-2 sm:block">
             {navLinks.map((link) => {
               return (
-                <Link key={link.title} href={link.href}>
-                  <span>{link.title}</span>
+                <Link key={link.title} href={link.href} className="">
+                  <Button variant="ghost">{link.title}</Button>
                 </Link>
               );
             })}
