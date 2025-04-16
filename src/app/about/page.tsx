be82683fc7929d85e2961491a40ea9560e3f1ca6
joey <969295728@qq.com>
2025-04-16 15:08:07 +0800

    docs(about): 在时间轴中添加博客上线日期

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,6 +14,7 @@ export default function About() {
         <div className="text-xl font-bold my-6 text-gray-500">TimeLine</div>
         <ul>
           <li>~ now - 施工中 🚧</li>
+          <li>2025.04.01 - 我的博客上线啦</li>
           <li>2024.11.01 - blogHub管理系统启动</li>
           <li>2024.10.14 - 前后端同时启动</li>
           <li>2024.10.01 - 技术选型启动</li>
